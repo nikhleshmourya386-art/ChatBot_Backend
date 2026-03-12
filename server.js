462081131b27a401ab087766455d095536ce4cad
Nikhilesh Mourya <nikhleshmourya386@gmail.com>
2026-03-12 16:56:32 +0530

    Clean commit without secrets

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ import cors from "cors";
 import chatRoute from "./route/chat.js";
 
 const app = express();
-
+const port=process.env.PORT||5000
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", chatRoute);
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server running on port 5000");
 });
