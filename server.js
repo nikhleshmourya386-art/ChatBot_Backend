71a446f6c74b78b68c3ae4d499db9d02af1be9c7
Nikhilesh Mourya <nikhleshmourya386@gmail.com>
2026-03-12 17:05:01 +0530

    agian

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
+// import express from "express";
+// import cors from "cors";
+// import chatRoute from "./route/chat.js";
+
+// const app = express();
+// const port=process.env.PORT||5000
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api", chatRoute);
+
+// app.listen(port, () => {
+//   console.log("Server running on port 5000");
+// });
+
+
 import express from "express";
 import cors from "cors";
 import chatRoute from "./route/chat.js";
 
 const app = express();
-const port=process.env.PORT||5000
+
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Chatbot Backend Running 🚀");
+});
+
 app.use("/api", chatRoute);
 
-app.listen(port, () => {
-  console.log("Server running on port 5000");
-});
+export default app;
