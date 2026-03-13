117375f9be8bed3ca644e5c0cc5af63a5c851ceb
Nikhilesh Mourya <nikhleshmourya386@gmail.com>
2026-03-13 22:18:49 +0530

    hello

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import chatRoute from "./routes/chat.js";
 
+dotenv.config();
+
 const app = express();
-dotenv.config()
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/",(req,res)=>{
-  res.send("Hello from my Side 💛")
-})
-
+// API route FIRST
 app.use("/api", chatRoute);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Hello from my Side 💛");
+});
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
