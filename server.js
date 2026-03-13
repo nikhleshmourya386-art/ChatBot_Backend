8d5d07b1a99fa51df2f0106f0c22e9be2cfe4748
Nikhilesh Mourya <nikhleshmourya386@gmail.com>
2026-03-13 21:51:38 +0530

    check it y;r

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+import dotenv from 'dotenv'
 import express from "express";
 import cors from "cors";
 import chatRoute from "./routes/chat.js";
 
 const app = express();
-
+dotenv.config()
 app.use(cors());
 app.use(express.json());
 
-app.use("/",(req,res)=>{
+app.use("/api/ch",(req,res)=>{
   res.send("Hello from my Side 💛")
 })
 
